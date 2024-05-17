b3673689a7d76ed7f839723a118999572d588bfb
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2024-05-17 16:10:57 +0800

    Efficient KAN Grafcet Reconstruct Update

--- KANPyTorch/EfficientKAN.py
+++ KANPyTorch/EfficientKAN.py
@@ -1,6 +1,7 @@
+import math
+
 import torch
 import torch.nn.functional as F
-import math
 
 
 class KANLinear(torch.nn.Module):
@@ -10,7 +11,6 @@ class KANLinear(torch.nn.Module):
             out_features,
             grid_size=5,
             spline_order=3,
-            scale_noise=0.1,
             scale_base=1.0,
             scale_spline=1.0,
             enable_standalone_scale_spline=True,
@@ -25,71 +25,54 @@ class KANLinear(torch.nn.Module):
         self.spline_order = spline_order
 
         # 構建網格點
-        h = (grid_range[1] - grid_range[0]) / grid_size
-        grid = (
-            (
-                    torch.arange(-spline_order, grid_size + spline_order + 1) * h
-                    + grid_range[0]
-            )
-            .expand(in_features, -1)
-            .contiguous()
-        )
-        self.register_buffer("grid", grid)
+        self.grid = self.build_grid(grid_range, grid_size, spline_order)
 
         # 初始化基礎權重和樣條權重
-        self.base_weight = torch.nn.Parameter(torch.Tensor(out_features, in_features))
-        self.spline_weight = torch.nn.Parameter(
-            torch.Tensor(out_features, in_features, grid_size + spline_order)
+        self.base_weight, self.spline_weight, self.spline_scaler = self.initialize_weights(
+            out_features, in_features, grid_size, spline_order, scale_base, scale_spline, enable_standalone_scale_spline
         )
-        if enable_standalone_scale_spline:
-            self.spline_scaler = torch.nn.Parameter(
-                torch.Tensor(out_features, in_features)
-            )
 
-        self.scale_noise = scale_noise
         self.scale_base = scale_base
         self.scale_spline = scale_spline
         self.enable_standalone_scale_spline = enable_standalone_scale_spline
         self.base_activation = base_activation()
         self.grid_eps = grid_eps
 
-        self.reset_parameters()
+    def build_grid(self, grid_range, grid_size, spline_order):
+        h = (grid_range[1] - grid_range[0]) / grid_size
+        grid = (
+            (
+                    torch.arange(-spline_order, grid_size + spline_order + 1) * h
+                    + grid_range[0]
+            )
+            .expand(self.in_features, -1)
+            .contiguous()
+        )
+        return grid
 
-    def reset_parameters(self):
-        # 用 Kaiming 初始化基礎權重,加入尺度因子
-        torch.nn.init.kaiming_uniform_(self.base_weight, a=math.sqrt(5) * self.scale_base)
-        with torch.no_grad():
-            # 用小的隨機噪聲初始化樣條權重
-            noise = (
-                    (
-                            torch.rand(self.grid_size + 1, self.in_features, self.out_features)
-                            - 1 / 2
-                    )
-                    * self.scale_noise
-                    / self.grid_size
+    def initialize_weights(self, out_features, in_features, grid_size, spline_order, scale_base, scale_spline,
+                           enable_standalone_scale_spline):
+        base_weight = torch.nn.Parameter(torch.Tensor(out_features, in_features))
+        spline_weight = torch.nn.Parameter(
+            torch.Tensor(out_features, in_features, grid_size + spline_order)
+        )
+        if enable_standalone_scale_spline:
+            spline_scaler = torch.nn.Parameter(
+                torch.Tensor(out_features, in_features)
             )
-            self.spline_weight.data.copy_(
-                (self.scale_spline if not self.enable_standalone_scale_spline else 1.0)
-                * self.curve2coeff(
-                    self.grid.T[self.spline_order: -self.spline_order],
-                    noise,
-                )
-            )
-            if self.enable_standalone_scale_spline:
-                torch.nn.init.kaiming_uniform_(self.spline_scaler, a=math.sqrt(5) * self.scale_spline)
+        else:
+            spline_scaler = None
+        torch.nn.init.kaiming_uniform_(base_weight, a=math.sqrt(5) * scale_base)
+        torch.nn.init.kaiming_uniform_(spline_weight, a=math.sqrt(5) * scale_spline)
+        if enable_standalone_scale_spline:
+            torch.nn.init.kaiming_uniform_(spline_scaler, a=math.sqrt(5) * scale_spline)
+        return base_weight, spline_weight, spline_scaler
 
     def b_splines(self, x: torch.Tensor):
-        """
-        計算給定輸入張量的 B 樣條基函數
+        bases = self.calculate_b_spline_bases(x)
+        return bases.contiguous()
 
-        Args:
-            x (torch.Tensor): 形狀為 (batch_size, in_features) 的輸入張量
-
-        Returns:
-            torch.Tensor: 形狀為 (batch_size, in_features, grid_size + spline_order) 的 B 樣條基函數張量
-        """
-        assert x.dim() == 2 and x.size(1) == self.in_features
-
+    def calculate_b_spline_bases(self, x: torch.Tensor):
         grid: torch.Tensor = (
             self.grid
         )  # (in_features, grid_size + 2 * spline_order + 1)
@@ -105,28 +88,9 @@ class KANLinear(torch.nn.Module):
                             / (grid[:, k + 1:] - grid[:, 1:(-k)])
                             * bases[:, :, 1:]
                     )
-
-        assert bases.size() == (
-            x.size(0),
-            self.in_features,
-            self.grid_size + self.spline_order,
-        )
-        return bases.contiguous()
+        return bases
 
     def curve2coeff(self, x: torch.Tensor, y: torch.Tensor):
-        """
-        計算插值給定點的曲線的係數
-
-        Args:
-            x (torch.Tensor): 形狀為 (batch_size, in_features) 的輸入張量
-            y (torch.Tensor): 形狀為 (batch_size, in_features, out_features) 的輸出張量
-
-        Returns:
-            torch.Tensor: 形狀為 (out_features, in_features, grid_size + spline_order) 的係數張量
-        """
-        assert x.dim() == 2 and x.size(1) == self.in_features
-        assert y.size() == (x.size(0), self.in_features, self.out_features)
-
         A = self.b_splines(x).transpose(
             0, 1
         )  # (in_features, batch_size, grid_size + spline_order)
@@ -137,40 +101,31 @@ class KANLinear(torch.nn.Module):
         result = solution.permute(
             2, 0, 1
         )  # (out_features, in_features, grid_size + spline_order)
-
-        assert result.size() == (
-            self.out_features,
-            self.in_features,
-            self.grid_size + self.spline_order,
-        )
         return result.contiguous()
 
     @property
     def scaled_spline_weight(self):
-        # 返回經過尺度變換的樣條權重
-        return self.spline_weight * (
-            self.spline_scaler.unsqueeze(-1)
-            if self.enable_standalone_scale_spline
-            else 1.0
-        )
+        if self.enable_standalone_scale_spline:
+            return self.spline_weight * self.spline_scaler.unsqueeze(-1)
+        else:
+            return self.spline_weight
 
     def forward(self, x: torch.Tensor):
-        assert x.dim() == 2 and x.size(1) == self.in_features
+        base_output = self.compute_base_output(x)
+        spline_output = self.compute_spline_output(x)
+        return base_output + spline_output
 
-        # 計算基礎部分和樣條部分的輸出,並相加
-        base_output = F.linear(self.base_activation(x), self.base_weight)
-        spline_output = F.linear(
+    def compute_base_output(self, x: torch.Tensor):
+        return F.linear(self.base_activation(x), self.base_weight)
+
+    def compute_spline_output(self, x: torch.Tensor):
+        return F.linear(
             self.b_splines(x).view(x.size(0), -1),
             self.scaled_spline_weight.view(self.out_features, -1),
         )
-        return base_output + spline_output
 
     @torch.no_grad()
     def update_grid(self, x: torch.Tensor, margin=0.01):
-        """
-        基於輸入數據更新網格點
-        """
-        assert x.dim() == 2 and x.size(1) == self.in_features
         batch = x.size(0)
 
         splines = self.b_splines(x)  # (batch, in, coeff)
@@ -182,7 +137,6 @@ class KANLinear(torch.nn.Module):
             1, 0, 2
         )  # (batch, in, out)
 
-        # 對每個通道單獨排序以收集數據分佈
         x_sorted = torch.sort(x, dim=0)[0]
         grid_adaptive = x_sorted[
             torch.linspace(
@@ -200,7 +154,6 @@ class KANLinear(torch.nn.Module):
                 - margin
         )
 
-        # 計算新的網格點
         grid = self.grid_eps * grid_uniform + (1 - self.grid_eps) * grid_adaptive
         grid = torch.concatenate(
             [
@@ -219,16 +172,6 @@ class KANLinear(torch.nn.Module):
         self.spline_weight.data.copy_(self.curve2coeff(x, unreduced_spline_output))
 
     def regularization_loss(self, regularize_activation=1.0, regularize_entropy=1.0):
-        """
-        計算正則化損失
-
-        這是對原文中提到的 L1 正則化的一個簡化模擬,因為原版需要從展開的
-        (batch, in_features, out_features) 中間張量計算絕對值和熵,
-        如果我們想要一個內存高效的實現,這個中間張量會被隱藏在 F.linear 函數後面。
-
-        L1 正則化現在被計算為樣條權重的平均絕對值。作者的實現中除了基於樣本的正則化外,
-        也包括了這一項。
-        """
         l1_fake = self.spline_weight.abs().mean(-1)
         regularization_loss_activation = l1_fake.sum()
         p = l1_fake / regularization_loss_activation
@@ -245,7 +188,6 @@ class KAN(torch.nn.Module):
             layers_hidden,
             grid_size=5,
             spline_order=3,
-            scale_noise=0.1,
             scale_base=1.0,
             scale_spline=1.0,
             base_activation=torch.nn.SiLU,
@@ -257,15 +199,20 @@ class KAN(torch.nn.Module):
         self.spline_order = spline_order
 
         # 構建 KAN 的層
-        self.layers = torch.nn.ModuleList()
+        self.layers = self.build_layers(
+            layers_hidden, grid_size, spline_order, scale_base, scale_spline, base_activation, grid_eps, grid_range
+        )
+
+    def build_layers(self, layers_hidden, grid_size, spline_order, scale_base, scale_spline, base_activation, grid_eps,
+                     grid_range):
+        layers = torch.nn.ModuleList()
         for in_features, out_features in zip(layers_hidden, layers_hidden[1:]):
-            self.layers.append(
+            layers.append(
                 KANLinear(
                     in_features,
                     out_features,
                     grid_size=grid_size,
                     spline_order=spline_order,
-                    scale_noise=scale_noise,
                     scale_base=scale_base,
                     scale_spline=scale_spline,
                     base_activation=base_activation,
@@ -273,9 +220,9 @@ class KAN(torch.nn.Module):
                     grid_range=grid_range,
                 )
             )
+        return layers
 
     def forward(self, x: torch.Tensor, update_grid=False):
-        # 前向傳播,可選擇是否更新網格點
         for layer in self.layers:
             if update_grid:
                 layer.update_grid(x)
@@ -283,7 +230,6 @@ class KAN(torch.nn.Module):
         return x
 
     def regularization_loss(self, regularize_activation=1.0, regularize_entropy=1.0):
-        # 計算所有層的正則化損失之和
         return sum(
             layer.regularization_loss(regularize_activation, regularize_entropy)
             for layer in self.layers
